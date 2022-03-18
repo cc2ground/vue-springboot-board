55c189530e1ebb5d14173b50b7a34e472d77d4be
git-groper <groper@cloocus.com>
2022-03-18 15:36:20 +0900

    [update: restart]

--- board-app/src/views/Home.vue
+++ board-app/src/views/Home.vue
@@ -1,6 +1,52 @@
 <template>
 	<div>
 		<v-container>
+			<v-card class="mx-auto" max-width="600" tile>
+				<v-card-title>
+					<h1>Welcome Cloocus Workshop</h1>
+				</v-card-title>
+				<v-card-text>
+					<h2>Introduction</h2>
+					<br />
+					Vue / Java Spring Boot / MySQL 3-tier Sample Project
+					<br />
+					<br />
+					<h2>Development Environment</h2>
+					<br />
+					<h3>APP</h3>
+					<ul>
+						<li>Vue</li>
+						<li>Vuetify</li>
+						<li>Vuex</li>
+						<li>Vue-Router</li>
+						<li>Axios</li>
+						<li>Toast UI Editor, Viewer</li>
+						<li>OpenWeatherMap</li>
+					</ul>
+					<br />
+					<h3>API</h3>
+					<ul>
+						<li>SpringBoot</li>
+						<li>MyBatis</li>
+						<li>Docker</li>
+						<li>MySQL</li>
+					</ul>
+				</v-card-text>
+				<v-img width="100%" src="../assets/programmer.png">
+					<v-row align="end" class="fill-height" style="margin: 0;">
+						<v-col class="pa-1">
+							<v-list-item color="rgba(0, 0, 0, .4)" dark class="pa-1">
+								<v-list-item-content>
+									<v-list-item-subtitle>Cloocus.com</v-list-item-subtitle>
+								</v-list-item-content>
+							</v-list-item>
+						</v-col>
+					</v-row>
+				</v-img>
+			</v-card>
+		</v-container>
+<!--
+		<v-container>
 			<v-card class="mx-auto" max-width="500" tile>
 				<v-card-title>
 					Vue, Spring Boot Board
@@ -38,22 +84,6 @@
 						<v-col class="pa-1">
 							<v-list-item color="rgba(0, 0, 0, .4)" dark class="pa-1">
 								<v-list-item-content>
-									<!--v-list-item-title class="title">
-										<a href="mailto:sun7190@naver.com">
-											<v-icon>mdi-email</v-icon>
-										</~a>
-										<a href="https://dev-jsk.tistory.com" target="_blank">
-											<v-icon>mdi-blogger</v-icon>
-										</a>
-										<a
-											href="https://github.com/jinseogood/vue-springboot-board"
-											target="_blank"
-										>
-											<v-icon>mdi-github</v-icon>
-										</a>
-										<br />
-										Jinseo Kim
-									</v-list-item-title-->
 									<v-list-item-subtitle>Cloocus.com</v-list-item-subtitle>
 								</v-list-item-content>
 							</v-list-item>
@@ -62,6 +92,7 @@
 				</v-img>
 			</v-card>
 		</v-container>
+-->
 	</div>
 </template>
 
